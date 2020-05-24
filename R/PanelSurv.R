f14b21d3f556a4328be4af51164029306fd30949
Sy Han (Steven) Chiou <schiou@utdallas.edu>
2020-05-24 04:00:02 +0000

    version 1.0.9

--- R/PanelSurv.R
+++ R/PanelSurv.R
@@ -1,3 +1,5 @@
+globalVariables(c("ID", "time2"))
+
 #' @name PanelSurv
 #' @rdname PanelSurv
 #' @title Create a PanelSurv Object
@@ -28,11 +30,7 @@
 #' indicating number of counts since last observation time.
 #'
 #' @seealso \code{\link{panelReg}}
-#' @examples
-#' data(blaTum)
-#' response <- with(blaTum, PanelSurv(id, time, count))
-#' is.PanelSurv(response)
-#' plot(response)
+#' @example inst/examples/ex_PanelSurv.R
 NULL
 
 #' @rdname PanelSurv
@@ -59,9 +57,44 @@ PanelSurv <- function(ID, time, count) {
 #' @export
 is.PanelSurv <- function(x) inherits(x, "PanelSurv")
 
+#' Produce Tile Plot
+#'
+#' Plot the tile plot from a \code{PanelSurv} object.
+#'
+#' @param x an object of class \code{PanelSurv}.
+#' @param heat an optional logical value indicating whether
+#' a swimmer-plot-like tile plot will be produced.
+#' @param order an optional logical value indicating whether the tile plot
+#' will be sorted by the largest observationt time.
+#' @param ... future extension
+#'
+#' @importFrom ggplot2 ylab xlab geom_bar coord_flip theme element_blank element_text
+#' @return A \code{ggplot} object
 #' @export
-plot.PanelSurv <- function(x, ...) {
-    with(x$psDF, ggplot(data = x$psDF, aes(time, ID, height = 2, width = 15)) +
-                 geom_tile(aes(fill = count)) + theme_bw() +
-                 scale_fill_gradient(low = "grey", high = "black"))
+plot.PanelSurv <- function(x, heat = FALSE, order = TRUE, ...) {
+    tmp <- aggregate(time ~ ID, length, data = x$psDF)[,2]
+    x$psDF$ID <- rep(1:length(tmp), tmp)
+    if (order) {
+        ranks <- rank(aggregate(time ~ ID, max, data = x$psDF)$time, ties.method = "first")
+        x$psDF$ID <- ranks[x$psDF$ID]
+        x$psDF <- x$psDF[order(x$psDF$ID),]
+    }
+    if (!heat) {
+        tileH <- 4500 / length(unique(x$psDF$ID))
+        ggplot(data = x$psDF, aes(time, ID, height = 2, width = tileH)) +
+            geom_tile(aes(fill = count)) + theme_bw() +
+            scale_fill_gradient(low = "grey", high = "black") +
+            ylab("Time") + xlab("Subject")
+    } else {
+        x$psDF$time2 <- with(x$ps, unlist(lapply(split(time, ID), function(x) diff(c(0, x)))))
+        ggplot(x$psDF, aes(ID, time2, fill = count)) +
+            geom_bar(stat = "identity", color = "black", size = .1) +
+            scale_fill_gradient(low = "gray80", high = "gray10") +
+            theme_bw() + coord_flip() +
+            theme(axis.line.y = element_blank(),
+                  axis.title.y = element_text(vjust = 0),
+                  axis.text.y = element_blank(),
+                  axis.ticks.y = element_blank()) +
+            ylab("Time") + xlab("Subject")
+    }
 }
